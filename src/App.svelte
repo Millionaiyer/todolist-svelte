c28b12185bc6e1c35ec30a9dcd1c2337c88c1277
millionaiyer <surajsaiyer@gmail.com>
2024-09-10 20:51:48 +0530

    initialization section4: chapter 22

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import ToDoList from './lib/ToDoList.svelte'
+  import {v4 as uuid} from 'uuid'
+
+  const toDos = [
+    {
+      id:uuid(),
+      title:'todo11',
+      completed: false,
+    },
+    {
+      id:uuid(),
+      title:'todo12',
+      completed: true,
+    },
+    {
+      id:uuid(),
+      title:'todo13',
+      completed: false,
+    },
+    {
+      id:uuid(),
+      title:'todo14',
+      completed: false,
+    }
+  ]
+</script>
+
+<div>
+  <ToDoList todos={toDos}/>
+</div>
---
+++ src/lib/ToDoList.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Button from "./Button.svelte";
+
+    export let todos = [];
+    let inputText = "";
+
+    const handleToDo = () => {
+        if (!inputText) return;
+        // console.log(inputText)
+    };
+</script>
+
+<div>
+    {#each todos as { id, title }, index (id)}
+        {@const number = index + 1}
+        <li>
+            {number} - {title}
+        </li>
+    {/each}
+
+    <form on:submit|preventDefault={handleToDo}>
+        <input bind:value={inputText} />
+        <!-- <Button type='submit' disabled={true}>Click me</Button> -->
+        <Button 
+        type="Submit" 
+        disabled={!inputText} 
+        on:click={() => console.log('hello')}
+      >Click</Button>
+      
+    </form>
+</div>
